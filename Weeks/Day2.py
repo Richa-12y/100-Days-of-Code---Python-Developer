cb431718da3fefc479273bb9cb9e21a8eaa26c63
Richa-12y <kricha000@gmail.com>
2023-09-21 23:21:59 +0530

    my second day code

---
+++ Weeks/Day2.py
@@ -0,0 +1,16 @@
+# add two number
+two_digit_number=input("Enter number what you want to add")
+
+first_digit=two_digit_number[0]
+second_digit=two_digit_number[1]
+result=int(first_digit)+int(second_digit)
+print(result)
+
+#who come first
+# ()
+# **
+# *
+# /
+# +
+# -
+print(3*3+3/3-3)
